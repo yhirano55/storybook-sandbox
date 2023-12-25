791210a11e5fe49426cdc686412e2c88d2064698
Yoshiyuki Hirano <yhirano@me.com>
2023-12-25 22:07:34 +0900

    Set useSWC

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,9 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+    builder: {
+      useSWC: true,
+    },
   },
   docs: {
     autodocs: "tag",
